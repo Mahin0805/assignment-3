d86c3137191c3afb8ad6ea3239b7de55a6178cc8
Mahin0805 <minhazmahin862@gmail.com>
2024-08-22 00:47:41 +0600

    responsive hero complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     theme: {
-      extend: {
-        // Custom classes for title, subtitle, and description
-        typography: {
-          title: {
-            fontSize: '2rem',
-            fontWeight: 'bold',
-            color: '#1E40AF',
-          },
-          subtitle: {
-            fontSize: '1.5rem',
-            fontWeight: '600',
-            color: '#1E3A8A',
-          },
-          description: {
-            fontSize: '1rem',
-            fontWeight: 'normal',
-            color: '#6B7280',
-          },
-        },
-      },
+      extend: {},
     },
     plugins: [],
   };
